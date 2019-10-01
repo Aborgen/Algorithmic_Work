b31dace74b69e447c534ff090ca14429abe422a3
Alexander Borgen <alexanderborgen@gmail.com>
2019-10-01 16:31:15 -0700

    Create quick-union implementation of UnionFind

---
+++ Algorithms/Part1/DataStructure/QuickUnionUF.h
@@ -0,0 +1,25 @@
+#ifndef QUICKUNIONUF_H
+#define QUICKUNIONUF_H
+#pragma once
+
+#include <vector>
+#include "UnionFind.h"
+
+namespace Part1::DataStructure
+{
+	class QuickUnionUF : UnionFind
+	{
+	public:
+		QuickUnionUF(int initialElements);
+		int count();
+		void push(int element);
+		void join(int p, int q);
+		bool anyConnection(int p, int q) const;
+		int operator[](int idx) const;
+
+	private:
+		std::vector<int> elements;
+		int getRoot(int idx) const;
+	};
+}
+#endif // !QUICKUNIONUF_H
---
+++ Algorithms/Part1/DataStructure/src/QuickUnionUF.cpp
@@ -0,0 +1,52 @@
+#include "../QuickUnionUF.h"
+
+namespace Part1::DataStructure
+{
+	QuickUnionUF::QuickUnionUF(int initialElements) {
+		for (int i = 0; i < initialElements; i++) {
+			push(i);
+		}
+	}
+
+	int QuickUnionUF::count()
+	{
+		return static_cast<int>(elements.size());
+	}
+
+	void QuickUnionUF::push(int element)
+	{
+		elements.push_back(element);
+	}
+
+	void QuickUnionUF::join(int p, int q)
+	{
+		int newRoot = getRoot(elements[p]);
+		int oldRoot = getRoot(elements[q]);
+		elements[oldRoot] = elements[newRoot];
+	}
+
+	bool QuickUnionUF::anyConnection(int p, int q) const
+	{
+		// A node is certainly connected to itself
+		if (p == q) {
+			return true;
+		}
+
+		return getRoot(p) == getRoot(q);
+	}
+
+	int QuickUnionUF::getRoot(int idx) const
+	{
+		int value = elements[idx];
+		if (value == idx) {
+			return value;
+		}
+
+		return getRoot(value);
+	}
+
+	int QuickUnionUF::operator[](int idx) const
+	{
+		return elements.at(idx);
+	}
+}
